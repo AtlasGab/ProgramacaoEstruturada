5c37cab4127f6f78847a58ccdbfe13dd4a8ae157
AtlasGab <emilygsa@gmail.com>
2024-11-14 11:21:35 -0300

    Add files via upload

---
+++ Estruturas de Repetição/Fibonacci.java
@@ -0,0 +1,26 @@
+package EstruturasDeRepeticao;
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Scanner ler=new Scanner(System.in);
+		int penultimo=0;
+		int ultimo=1;
+		int aux;
+		
+		System.out.println("Posição da sequência de Fibonacci:");
+		int posicao=ler.nextInt();
+		
+		for(int i=posicao;i>=1;i--) {
+			aux=penultimo+ultimo;
+			penultimo=ultimo;
+			ultimo=aux;
+		}
+		
+		System.out.println(penultimo);
+
+		ler.close();
+	}
+
+}
